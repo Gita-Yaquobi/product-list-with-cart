dfaf0fd95b00980877e0c3be4e9d5b7450825579
Gita-Yaquobi <yaquobi.biz@gmail.com>
2025-02-18 11:08:43 +0330

    getData from data.json

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,22 @@
 const $ = document;
-getData("http://localhost/data.json", updateCardsCon);
-
+getData("./data.json", updateCardsCon);
 
 function getData(url, callback) {
-
   fetch(url)
-    .then((response) => response.json())
-    .then(function (res) {
-
-      callback(res);
-
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      callback(data);
+    })
+    .catch((error) => {
+      console.error('There has been a problem with your fetch operation:', error);
+      callback([]);
     });
-
 }
-
 function updateImageSources() {
 
   $.querySelectorAll('.card-img-top').forEach(img => {
